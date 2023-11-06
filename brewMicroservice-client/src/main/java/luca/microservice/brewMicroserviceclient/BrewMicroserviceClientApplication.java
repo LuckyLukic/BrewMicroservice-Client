f15d483af7761f545a46e8d6a399e8fe87c044b0
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-11-06 11:18:26 +0100

    starter

---
+++ brewMicroservice-client/src/main/java/luca/microservice/brewMicroserviceclient/BrewMicroserviceClientApplication.java
@@ -0,0 +1,13 @@
+package luca.microservice.brewMicroserviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewMicroserviceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewMicroserviceClientApplication.class, args);
+	}
+
+}
---
+++ brewMicroservice-client/src/test/java/luca/microservice/brewMicroserviceclient/BrewMicroserviceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package luca.microservice.brewMicroserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewMicroserviceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
